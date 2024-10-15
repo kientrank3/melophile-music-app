8a6176d8a199d5c9bfb5e0725b77bd274928c2ef
kientrank3 <bankienthanthien@gmail.com>
2024-10-15 07:13:48 +0700

    Add drawer cho trang home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,25 @@ import { AlbumListItem } from "@/components/AlbumListItem";
 import { ArtistItem } from "@/components/ArtistItem";
 import { TrackListGenre } from "@/components/TrackListGenre";
 import { TrackListItem } from "@/components/TrackListItem";
+import {
+  Drawer,
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+} from "@/components/ui/drawer";
 import { Genre } from "@/utils/database.types";
 import supabase from "@/utils/supabase";
-import { BellRing, CirclePlay, History, Settings } from "lucide-react-native";
+import {
+  BellRing,
+  CirclePlay,
+  History,
+  HistoryIcon,
+  Plus,
+  Settings,
+  Zap,
+} from "lucide-react-native";
 import { useEffect, useState } from "react";
 import {
   View,
@@ -99,6 +115,7 @@ const albums = [
   },
 ];
 const HomeScreen = () => {
+  const [showDrawer, setShowDrawer] = useState(false);
   const [genres, setGenres] = useState<Genre[]>([]); // Khai báo state với kiểu Genre[]
 
   useEffect(() => {
@@ -118,7 +135,11 @@ const HomeScreen = () => {
   return (
     <ScrollView stickyHeaderIndices={[0]} className="mt-4">
       <View className="p-2 pb-2 flex-row justify-between items-center bg-black">
-        <TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setShowDrawer(true);
+          }}
+        >
           <Image
             className="w-10 h-10 rounded-full"
             source={{
@@ -139,6 +160,56 @@ const HomeScreen = () => {
           </TouchableOpacity>
         </View>
       </View>
+      <Drawer
+        isOpen={showDrawer}
+        onClose={() => {
+          setShowDrawer(false);
+        }}
+        size="lg"
+        anchor="left"
+      >
+        <DrawerBackdrop />
+        <DrawerContent>
+          <DrawerHeader>
+            <TouchableOpacity className="flex-row items-center w-full">
+              <Image
+                className="w-10 h-10 rounded-full"
+                source={{
+                  uri: "https://i.vietgiaitri.com/2024/4/27/anh-trai-say-hi-hoi-tu-loat-ten-tuoi-cuc-hot-vi-sao-lai-co-30-thi-sinh-271-7150873.jpg",
+                }}
+                alt="image"
+              />
+              <View className="pl-2">
+                <Text className="text-white font-xl font-semibold">
+                  Trần Đình Kiên
+                </Text>
+                <Text className="text-gray-400 font-sm">Xem hồ sơ</Text>
+              </View>
+            </TouchableOpacity>
+          </DrawerHeader>
+          <DrawerBody className="border-t border-gray-700">
+            <TouchableOpacity className="flex-row items-center py-2">
+              <Plus color={"#fff"} size={20} />
+              <Text className="text-white font-xl pl-2">Thêm tài khoản</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-row items-center py-2">
+              <Zap color={"#fff"} size={20} />
+              <Text className="text-white font-xl pl-2">Thông tin mới</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-row items-center py-2">
+              <HistoryIcon color={"#fff"} size={20} />
+              <Text className="text-white font-xl pl-2">Nội dung đã nghe</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="flex-row items-center py-2">
+              <Settings color={"#fff"} size={20} />
+              <Text className="text-white font-xl pl-2">
+                Cài đặt và quyền riêng tư
+              </Text>
+            </TouchableOpacity>
+          </DrawerBody>
+          <DrawerFooter></DrawerFooter>
+        </DrawerContent>
+      </Drawer>
       <View className="p-2">
         <Text className="text-white text-xl font-bold">
           Nghệ sĩ yêu thích của bạn
